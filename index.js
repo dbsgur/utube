91f4ce7f1ee2a7529535d1f898a023768acb0ace
dbsgur <kkr431@naver.com>
2022-01-09 19:30:14 +0900

    mongo connect

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+//서버에서 가져온 데이터를 파싱해서 가져와준다.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
@@ -19,6 +26,16 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
