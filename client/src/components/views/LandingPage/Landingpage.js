6d4aaa326c26913c94242171b462a56bbf9a356b
dbsgur <kkr431@naver.com>
2022-01-10 15:41:38 +0900

    register Page frontend + logout frontend

--- client/src/components/views/LandingPage/Landingpage.js
+++ client/src/components/views/LandingPage/Landingpage.js
@@ -1,13 +1,19 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Landingpage() {
-  useEffect(() => {
-    axios.get(`/api/hello`).then((response) => {
-      console.log(response);
-    });
-  }, []);
+  const navigate = useNavigate();
 
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        navigate("/login");
+      } else {
+        alert("Logout Fail");
+      }
+    });
+  };
   return (
     <div
       style={{
@@ -19,6 +25,7 @@ function Landingpage() {
       }}
     >
       <h1>Landingpage</h1>
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
